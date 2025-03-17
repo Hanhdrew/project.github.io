9a9127adac8270475334cd9c1860fa258d751b91
Hanhdrew <andrew.rebbe@gmail.com>
2025-03-17 14:20:58 -0700

    reverted js css

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
 const videoCards = document.querySelectorAll('.video-card');
 
-const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-
 const observerOptions = {
-  root: null, 
-  rootMargin: '0px', 
-  threshold: 0.5, 
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
 };
 
 const observerCallback = (entries, observer) => {
@@ -13,21 +11,9 @@ const observerCallback = (entries, observer) => {
     if (entry.isIntersecting) {
       entry.target.style.opacity = '1';
       entry.target.style.transform = 'translateY(0)';
-
-      if (isTouchDevice) {
-        entry.target.style.transform = 'translateY(0) scale(1.05)';
-        entry.target.style.color = 'var(--clr-highlight)';
-        entry.target.style.border = '3px solid var(--clr-highlight)';
-      }
     } else {
       entry.target.style.opacity = '0';
       entry.target.style.transform = 'translateY(20px)';
-
-      if (isTouchDevice) {
-        entry.target.style.transform = 'translateY(20px) scale(1)';
-        entry.target.style.color = ''; 
-        entry.target.style.border = ''; 
-      }
     }
   });
 };
